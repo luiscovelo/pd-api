204fb56d485a2fa7bd17f6d6b466da5159e8ec21
Luis Covelo <covelo.luis@hotmail.com>
2022-02-15 00:54:30 -0300

    fix docker-compose up

---
+++ db/01-init.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -e
+export PGPASSWORD=postgres;
+psql -v ON_ERROR_STOP=1 --username "postgres" --dbname "pdhours" <<-EOSQL
+  CREATE DATABASE pdhours;
+  \connect pdhours postgres
+  BEGIN;
+  create table squad(
+		id serial primary key,
+		name character varying(255) not null
+	);
+
+	create table employee(
+		id serial primary key,
+		name character varying(255) not null,
+		estimated_hours integer not null,
+		squad_id integer not null,
+		FOREIGN KEY (squad_id) REFERENCES squad(id)
+	);
+
+	create table report(
+		id serial primary key,
+		created_at date not null,
+		description text not null,
+		spent_hours integer not null,
+		employee_id integer not null,
+		FOREIGN KEY (employee_id) REFERENCES employee(id)
+	);
+  COMMIT;
+EOSQL
--- schema.sql
+++ schema.sql
@@ -1,21 +1,21 @@
 create table squad(
-	id int primary key auto_increment,
-	name varchar(255) not null
+	id serial primary key,
+	name character varying(255) not null
 );
 
 create table employee(
-	id int primary key auto_increment,
-	name varchar(255) not null,
-	estimated_hours int not null,
-	squad_id int not null,
+	id serial primary key,
+	name character varying(255) not null,
+	estimated_hours integer not null,
+	squad_id integer not null,
 	FOREIGN KEY (squad_id) REFERENCES squad(id)
 );
 
 create table report(
-	id int primary key auto_increment,
+	id serial primary key,
 	created_at date not null,
 	description text not null,
-	spent_hours int not null,
-	employee_id int not null,
+	spent_hours integer not null,
+	employee_id integer not null,
 	FOREIGN KEY (employee_id) REFERENCES employee(id)
 );
